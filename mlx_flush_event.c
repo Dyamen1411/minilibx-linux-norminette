b9fcdd22a58c838f75b28f5220f5732c24d20c74
Dyamen1411 <massias.antoine@gmail.com>
2023-09-04 01:52:54 +0200

    mlx_flush_event.c is fully norminette friendly

--- mlx_flush_event.c
+++ mlx_flush_event.c
@@ -1,25 +1,21 @@
-/*
-** mlx_flush_event.c for MiniLibX in 
-** 
-** Made by Charlie Root
-** Login   <ol@epitech.net>
-** 
-** Started on  Wed Aug  2 18:58:11 2000 Charlie Root
-** Last update Fri Feb 23 17:08:48 2001 Charlie Root
-*/
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_flush_event.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dyamen <dyamen@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 01:51:57 by Charlie Roo       #+#    #+#             */
+/*   Updated: 2023/09/04 01:52:38 by dyamen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-
-#include	"mlx_int.h"
-
-
-
+#include "mlx_int.h"
 
 int	mlx_flush_event(t_xvar *xvar)
 {
-  XEvent	ev;
+	XEvent	ev;
 
-  while (XPending(xvar->display))
-  {
-     XNextEvent(xvar->display,&ev);
-  }
+	while (XPending(xvar->display))
+		XNextEvent(xvar->display, &ev);
 }
