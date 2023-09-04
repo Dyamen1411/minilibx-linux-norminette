8ed48b4fdaf2b029d9abaff39a19b1af374e4253
Dyamen1411 <massias.antoine@gmail.com>
2023-09-04 04:17:17 +0200

    mlx_string_put.c is mostly norminette friendly

--- mlx_string_put.c
+++ mlx_string_put.c
@@ -1,26 +1,26 @@
-/*
- ** mlx_string_put.c for MiniLibX in 
- ** 
- ** Made by Charlie Root
- ** Login   <ol@epitech.net>
- ** 
- ** Started on  Mon Jul 31 19:01:33 2000 Charlie Root
-** Last update Tue Sep 25 17:11:47 2001 Charlie Root
- */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_string_put.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dyamen <dyamen@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 04:16:08 by Charlie Roo       #+#    #+#             */
+/*   Updated: 2023/09/04 04:16:57 by dyamen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "mlx_int.h"
 
-#include	"mlx_int.h"
-
-
-
-int		mlx_string_put(t_xvar *xvar,t_win_list *win,
-			       int x,int y,int color,char *string)
+int	mlx_string_put(t_xvar *xvar, t_win_list *win,
+		int x, int y, int color, char *string)
 {
-   XGCValues	xgcv;
-   
-   xgcv.foreground = mlx_int_get_good_color(xvar,color);
-   XChangeGC(xvar->display,win->gc,GCForeground,&xgcv);
-   XDrawString(xvar->display,win->window,win->gc,x,y,string,strlen(string));
-   if (xvar->do_flush)
-     XFlush(xvar->display);
+	XGCValues	xgcv;
+
+	xgcv.foreground = mlx_int_get_good_color(xvar, color);
+	XChangeGC(xvar->display, win->gc, GCForeground, &xgcv);
+	XDrawString(xvar->display, win->window, win->gc, x, y,
+		string, strlen(string));
+	if (xvar->do_flush)
+		XFlush(xvar->display);
 }
