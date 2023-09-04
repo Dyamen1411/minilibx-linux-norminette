14d55f7f149f8f87bf0ecb696efca729e4fa67c7
Dyamen1411 <massias.antoine@gmail.com>
2023-09-04 02:58:25 +0200

    mlx_loop_hook.c is fully norminette friendly

--- mlx_loop_hook.c
+++ mlx_loop_hook.c
@@ -1,21 +1,19 @@
-/*
-** mlx_loop_hook.c for MiniLibX in 
-** 
-** Made by Charlie Root
-** Login   <ol@epitech.net>
-** 
-** Started on  Thu Aug  3 11:49:06 2000 Charlie Root
-** Last update Fri Feb 23 17:11:39 2001 Charlie Root
-*/
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_loop_hook.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dyamen <dyamen@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 02:57:53 by Charlie roo       #+#    #+#             */
+/*   Updated: 2023/09/04 02:58:13 by dyamen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "mlx_int.h"
 
-#include	"mlx_int.h"
-
-
-
-
-int	mlx_loop_hook(t_xvar *xvar,int (*funct)(),void *param)
+int	mlx_loop_hook(t_xvar *xvar, int (*funct)(), void *param)
 {
-  xvar->loop_hook = funct;
-  xvar->loop_param = param;
+	xvar->loop_hook = funct;
+	xvar->loop_param = param;
 }
