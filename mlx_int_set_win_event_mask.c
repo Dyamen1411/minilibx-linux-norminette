f9a66a31969a1f7b5b8224ee7c0529deef58f73e
Dyamen1411 <massias.antoine@gmail.com>
2023-09-04 02:25:17 +0200

    mlx_int_set_win_event_mask.c is fully norminette friendly

--- mlx_int_set_win_event_mask.c
+++ mlx_int_set_win_event_mask.c
@@ -1,34 +1,32 @@
-/*
-** mlx_int_set_win_event_mask.c for MiniLibX in 
-** 
-** Made by Charlie Root
-** Login   <ol@epitech.net>
-** 
-** Started on  Thu Aug  3 11:49:06 2000 Charlie Root
-** Last update Fri Feb 23 17:07:42 2001 Charlie Root
-*/
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_int_set_win_event_mask.c                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dyamen <dyamen@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 02:23:44 by Charlie Roo       #+#    #+#             */
+/*   Updated: 2023/09/04 02:24:55 by dyamen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-
-#include	"mlx_int.h"
-
-
-
+#include "mlx_int.h"
 
 int	mlx_int_set_win_event_mask(t_xvar *xvar)
 {
-  t_win_list	*win;
-  int		mask;
-  int		i;
-  XSetWindowAttributes	xwa;
+	t_win_list				*win;
+	int						mask;
+	int						i;
+	XSetWindowAttributes	xwa;
 
-  win = xvar->win_list;
-  while (win)
-    {
-      xwa.event_mask = 0;
-      i = MLX_MAX_EVENT;
-      while (i--)
-	xwa.event_mask |= win->hooks[i].mask;
-      XChangeWindowAttributes(xvar->display, win->window, CWEventMask, &xwa);
-      win = win->next;
-    }
+	win = xvar->win_list;
+	while (win)
+	{
+		xwa.event_mask = 0;
+		i = MLX_MAX_EVENT;
+		while (i--)
+			xwa.event_mask |= win->hooks[i].mask;
+		XChangeWindowAttributes(xvar->display, win->window, CWEventMask, &xwa);
+		win = win->next;
+	}
 }
