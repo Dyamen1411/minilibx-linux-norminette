d7f566bb391d2259b9cc0aff1a6a38e3d24afd06
Dyamen1411 <massias.antoine@gmail.com>
2023-09-04 04:04:04 +0200

    mlx_new_window.c is fully norminette friendly

--- mlx_new_window.c
+++ mlx_new_window.c
@@ -1,13 +1,14 @@
-/*
-** mlx_new_window.c for MiniLibX in 
-** 
-** Made by Charlie Root
-** Login   <ol@epitech.net>
-** 
-** Started on  Mon Jul 31 17:29:02 2000 Charlie Root
-** Last update Thu Oct  4 15:44:43 2001 Charlie Root
-*/
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_new_window.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dyamen <dyamen@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 04:03:19 by Charlie Roo       #+#    #+#             */
+/*   Updated: 2023/09/04 04:03:33 by dyamen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 /*
 ** We do not use White/BlackPixel macro, TrueColor Visual make sure
@@ -16,47 +17,52 @@
 ** With mlx_int_wait_first_expose, no flush is needed.
 */
 
-#include	"mlx_int.h"
+#include "mlx_int.h"
 
-
-void	*mlx_new_window(t_xvar *xvar,int size_x,int size_y,char *title)
-{
-	t_win_list				*new_win;
+typedef struct s_window_settings {
+	t_win_list				*win;
 	XSetWindowAttributes	xswa;
 	XGCValues				xgcv;
+	int						width;
+	int						height;
+	char					*title;
+}	t_window_settings;
 
-	xswa.background_pixel = 0;
-	xswa.border_pixel = -1;
-	xswa.colormap = xvar->cmap;
-	/*
-	xswa.event_mask = ButtonPressMask | ButtonReleaseMask | ExposureMask |
-		KeyPressMask | KeyReleaseMask | StructureNotifyMask;
-	*/
-	/* xswa.event_mask = ExposureMask; */
-	xswa.event_mask = 0xFFFFFF;	/* all events */
-	if (!(new_win = malloc(sizeof(*new_win))))
-		return ((void *)0);
-	new_win->window = XCreateWindow(xvar->display,xvar->root,0,0,size_x,size_y,
-					0,CopyFromParent,InputOutput,xvar->visual,
-					CWEventMask|CWBackPixel|CWBorderPixel|
-					CWColormap,&xswa);
-	mlx_int_anti_resize_win(xvar,new_win->window,size_x,size_y);
-	XStoreName(xvar->display,new_win->window,title);
-	XSetWMProtocols(xvar->display, new_win->window, &(xvar->wm_delete_window), 1);
-	xgcv.foreground = -1;
-	xgcv.function = GXcopy;
-	xgcv.plane_mask = AllPlanes;
-	new_win->gc = XCreateGC(xvar->display,new_win->window,
-				GCFunction|GCPlaneMask|GCForeground,&xgcv);
-	new_win->next = xvar->win_list;
-	xvar->win_list = new_win;
-	/*
-	new_win->mouse_hook = mlx_int_do_nothing;
-	new_win->key_hook = mlx_int_do_nothing;
-	new_win->expose_hook = mlx_int_do_nothing;
-	*/
-	bzero(&(new_win->hooks), sizeof(new_win->hooks));
-	XMapRaised(xvar->display,new_win->window);
-	mlx_int_wait_first_expose(xvar,new_win->window);
-	return (new_win);
+void	create_window(t_window_settings *ws, t_xvar *xvar)
+{
+	ws->win->window = XCreateWindow(xvar->display, xvar->root, 0, 0,
+			ws->width, ws->height,
+			0, CopyFromParent, InputOutput, xvar->visual,
+			CWEventMask | CWBackPixel | CWBorderPixel | CWColormap, &ws->xswa);
+	mlx_int_anti_resize_win(xvar, ws->win->window, ws->width, ws->height);
+	XStoreName(xvar->display, ws->win->window, ws->title);
+	XSetWMProtocols(xvar->display, ws->win->window, &xvar->wm_delete_window, 1);
+}
+
+void	*mlx_new_window(t_xvar *xvar, int size_x, int size_y, char *title)
+{
+	t_window_settings	ws;
+
+	ws.win = malloc(sizeof(t_win_list));
+	if (!ws.win)
+		return ((void *) 0);
+	ws.width = size_x;
+	ws.height = size_y;
+	ws.title = title;
+	ws.xswa.background_pixel = 0;
+	ws.xswa.border_pixel = -1;
+	ws.xswa.colormap = xvar->cmap;
+	ws.xswa.event_mask = 0xFFFFFF;
+	ws.xgcv.foreground = -1;
+	ws.xgcv.function = GXcopy;
+	ws.xgcv.plane_mask = AllPlanes;
+	create_window(&ws, xvar);
+	ws.win->gc = XCreateGC(xvar->display, ws.win->window,
+			GCFunction | GCPlaneMask | GCForeground, &ws.xgcv);
+	ws.win->next = xvar->win_list;
+	xvar->win_list = ws.win;
+	bzero(&ws.win->hooks, sizeof(ws.win->hooks));
+	XMapRaised(xvar->display, ws.win->window);
+	mlx_int_wait_first_expose(xvar, ws.win->window);
+	return (ws.win);
 }
