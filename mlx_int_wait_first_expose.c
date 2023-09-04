dacc48c4772bd3c25be15faac5e1dbadda593a20
Dyamen1411 <massias.antoine@gmail.com>
2023-09-04 02:43:25 +0200

    mlx_int_wait_first_expose.c is fully norminette friendly

--- mlx_int_wait_first_expose.c
+++ mlx_int_wait_first_expose.c
@@ -1,23 +1,21 @@
-/*
-** mlx_int_wait_first_expose.c for minilibx in 
-** 
-** Made by olivier crouzet
-** Login   <ol@epita.fr>
-** 
-** Started on  Tue Oct 17 09:26:45 2000 olivier crouzet
-** Last update Fri Feb 23 17:27:10 2001 Charlie Root
-*/
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_int_wait_first_expose.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dyamen <dyamen@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 02:42:11 by ocrouzet          #+#    #+#             */
+/*   Updated: 2023/09/04 02:42:59 by dyamen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "mlx_int.h"
 
-
-#include	"mlx_int.h"
-
-
-
-int	mlx_int_wait_first_expose(t_xvar *xvar,Window win)
+int	mlx_int_wait_first_expose(t_xvar *xvar, Window win)
 {
-  XEvent	ev;
+	XEvent	ev;
 
-  XWindowEvent(xvar->display,win,ExposureMask,&ev);
-  XPutBackEvent(xvar->display,&ev);
+	XWindowEvent(xvar->display, win, ExposureMask, &ev);
+	XPutBackEvent(xvar->display, &ev);
 }
