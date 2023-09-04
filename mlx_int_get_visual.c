1b8205dccb7ab910ce60cc6c55fb450f2d30a4f1
Dyamen1411 <massias.antoine@gmail.com>
2023-09-04 02:13:40 +0200

    mlx_int_get_visual.c is fully norminette friendly

--- mlx_int_get_visual.c
+++ mlx_int_get_visual.c
@@ -1,39 +1,36 @@
-/*
-** mlx_int_get_visual.c for MinilibX in 
-** 
-** Made by Charlie Root
-** Login   <ol@epitech.net>
-** 
-** Started on  Wed Oct  3 17:01:51 2001 Charlie Root
-** Last update Thu Oct  4 15:00:45 2001 Charlie Root
-*/
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_int_get_visual.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dyamen <dyamen@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 02:12:07 by Charlie Roo       #+#    #+#             */
+/*   Updated: 2023/09/04 02:13:18 by dyamen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "mlx_int.h"
 
+// We need a private colormap for non-default Visual.
 
-#include	"mlx_int.h"
-
-
-/*
-** We need a private colormap for non-default Visual.
-*/
-
-
-int		mlx_int_get_visual(t_xvar *xvar)
+int	mlx_int_get_visual(t_xvar *xvar)
 {
-  XVisualInfo	*vi;
-  XVisualInfo	template;
-  int		nb_item;
+	XVisualInfo	*vi;
+	XVisualInfo	template;
+	int			nb_item;
 
-  xvar->private_cmap = 0;
-  xvar->visual = DefaultVisual(xvar->display,xvar->screen);
-  if (xvar->visual->class == TrueColor)
-    return (0);
-  template.class = TrueColor;
-  template.depth = xvar->depth;
-  if (!(vi = XGetVisualInfo(xvar->display,VisualDepthMask|VisualClassMask,
-			    &template,&nb_item)) )
-    return (-1);
-  xvar->visual = vi->visual;
-  xvar->private_cmap = 1;
-  return (0);
+	xvar->private_cmap = 0;
+	xvar->visual = DefaultVisual(xvar->display, xvar->screen);
+	if (xvar->visual->class == TrueColor)
+		return (0);
+	template.class = TrueColor;
+	template.depth = xvar->depth;
+	vi = XGetVisualInfo(xvar->display, VisualDepthMask | VisualClassMask,
+			&template, &nb_item);
+	if (!vi)
+		return (-1);
+	xvar->visual = vi->visual;
+	xvar->private_cmap = 1;
+	return (0);
 }
